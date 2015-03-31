f288a10660106debaea9265e280a66e8b8db35a0
Giovanni Le Grand <g.le.grand@Mondriaanict.nl>
2015-03-31 08:50:02 +0200

    dingen veranderd

--- Soundboard/Soundboard/Form1.cs
+++ Soundboard/Soundboard/Form1.cs
@@ -9,6 +9,9 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Soundboard.Media;
 using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Reflection;
 
 namespace Soundboard
 {
@@ -17,6 +20,11 @@ namespace Soundboard
         public Form1()
         {
             InitializeComponent();
+            XmlSerializer xser = new XmlSerializer(typeof(Buttons));
+            string[] test = this.GetType().Assembly.GetManifestResourceNames();
+            Stream xmlstream = this.GetType().Assembly.GetManifestResourceStream("Soundboard.Resources.Buttons.xml");
+            var buttons = (Buttons) xser.Deserialize(xmlstream);
+            Console.WriteLine(buttons.knoppen.Count());
         }
 
         private void button1_Click(object sender, EventArgs e)
